d3f61d9f0f10c47ebf33cd979c00239bc087bb1f
Dorian Beaumaikers <dorianbeaumaikers@gmail.com>
2022-02-10 18:16:27 +0100

    Voyager Migrations

---
+++ database/migrations/2022_02_10_171621_create_works_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->string('image', 45)->nullable();
+            $table->tinyInteger('inSlider');
+            $table->timestamp('created_at')->useCurrent()->useCurrentOnUpdate();
+            $table->timestamp('updated_at')->nullable();
+            $table->unsignedInteger('client_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
